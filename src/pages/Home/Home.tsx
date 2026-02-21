f5d599407f03faa35a7b714cb1099b6e3b829fc2
Ronald Kauan <kauanpaz49@gmail.com>
2026-02-21 18:20:25 -0400

    feat: Update home data to use constants

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import Welcome from "../../assets/welcome.svg";
 
+const USER = "Millena";
+const DATE = "22, Novembro 2024";
+
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col gap-3">
       <h1 className="text-[2.375rem] font-bold">Home</h1>
 
       <div className="bg-white pt-6 px-7 shadow-[0px_1px_4px_#00000029] rounded-[5px]">
-        <h2 className="text-[2rem] font-bold text-[#0D1931]">Olá Millena!</h2>
-        <h3 className="text-[1.125rem] font-semibold text-[#0D1931]">
-          22, Novembro 2024
-        </h3>
+        <h2 className="text-[2rem] font-bold text-[#0D1931]">Olá {USER}!</h2>
+        <h3 className="text-[1.125rem] font-semibold text-[#0D1931]">{DATE}</h3>
 
         <div className="flex flex-col gap-4 mt-20 mb-30">
           <div className="self-center">
